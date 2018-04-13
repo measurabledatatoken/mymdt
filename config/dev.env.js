3bc3f049b3482c9f7219fcfeb62ad19483a7b4fb
gem <gem@mailtime.com>
2018-04-13 16:37:20 +0800

    fix typo

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_ENDPOINT: '"locolhost:8080"',
+  API_ENDPOINT: '"localhost:8080"',
   API_Scheme: '"http"',
 })
