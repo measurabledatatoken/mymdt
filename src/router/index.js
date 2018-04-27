be351948455505e60fd30c889b4184204b2fc7c9
gem <gem@mailtime.com>
2018-04-27 18:34:38 +0800

    remove the # in url and route to home for unknown route

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,11 @@ import Home from '@/components/Home';
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
+    { // Route to Home page for route not defined
+      path: '*', component: Home,
+    },
     {
       path: '/',
       name: 'home',
