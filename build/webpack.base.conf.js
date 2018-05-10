756a14b8334b42d665e4f4354e868a5ab89cb9da
gem <gem@mailtime.com>
2018-05-10 16:51:59 +0800

    scss already supported in utils.js

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -74,14 +74,6 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
       }
     ]
   },
