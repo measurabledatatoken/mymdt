6c4b808aef952acd0089c22a14f746e6c1e93077
Maxi Cho <maxi@mailtime.com>
2018-06-08 15:09:33 +0800

    fix virtual keyboard glitch

---
+++ src/helpers/fixVirtualKeyboardGlitch.js
@@ -0,0 +1,18 @@
+export default function fixVirtualKeyboardGlitch() {
+  if (/iPad|iPhone|iPod/.test(navigator.userAgent)) {
+    const getScrollingElement = function getScrollingElement() {
+      return document.scrollingElement || document.documentElement;
+    };
+    let scrollTop = getScrollingElement().scrollTop;
+    const handleScroll = function handleScroll() {
+      const el = getScrollingElement();
+      const nextScrollTop = el.scrollTop;
+      if (nextScrollTop <= scrollTop) { // keyboard is hidden
+        el.scrollTo(0, 0);
+      }
+
+      scrollTop = nextScrollTop;
+    };
+    window.addEventListener('scroll', handleScroll, false);
+  }
+}
