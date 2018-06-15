f39da154834c11770b2e9bf21c1150dacfe599d1
Maxi Cho <maxi@mailtime.com>
2018-06-15 14:09:15 +0800

    add base component: 1. Secondary button 2. Skeleton (for skeleton loading screen)

---
+++ src/components/button/MDTSecondaryButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <md-button :class="['secondary-button', { 'secondary-button__color--secondary': color === 'secondary' }]" v-bind="$attrs" v-on="$listeners">
+    <slot></slot>
+  </md-button>
+</template>
+
+<script>
+export default {
+  props: ['color'],
+};
+</script>
+
+<style lang="scss" scoped>
+.md-button {
+  &.secondary-button {
+    height: auto;
+    min-width: 6.5625rem;
+    font-size: 0.875rem;
+    line-height: 1.25rem;
+    padding: 0.125rem 0.375rem;
+    color: white;
+    background-color: $theme-color;
+    border-radius: 1rem;
+
+    &.secondary-button__color--secondary {
+      background-color: $theme-secondary-color;
+    }
+  }
+}
+</style>
+
---
+++ src/components/common/Skeleton.vue
@@ -0,0 +1,51 @@
+<template>
+  <div :style="{ width, height }" class="content-placeholder-text">
+    <div class="content-placeholder__background"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    width: {
+      type: String,
+      default: '100%',
+    },
+    height: {
+      type: String,
+      default: '0.5rem',
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.content-placeholder-text {
+  display: block;
+  background-color: #ebebeb;
+  overflow: hidden;
+  position: relative;
+  border-radius: 0.125rem;
+  margin: 0.25rem 0;
+
+  .content-placeholder__background {
+    animation: content-placeholder-animation 1s linear infinite;
+    background: linear-gradient(to right, rgba(15,15,15,0) 0%,rgba(219,219,219,0.4) 50%,rgba(255,255,255,0) 100%);
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 70%;
+  }
+}
+
+@keyframes content-placeholder-animation {
+  0% {
+    transform: translateX(-50%);
+  }
+
+  100% {
+    transform: translateX(300%);
+  }
+}
+</style>
