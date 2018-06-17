88b5730f0a102bb60551a15c33fcee8aec1b8516
gem <gem@mailtime.com>
2018-06-17 10:59:05 +0800

    add back phone number setup

--- src/constants/index.js
+++ src/constants/index.js
@@ -38,9 +38,9 @@ const RouteDef = {
     path: '/home/accounts/:account_id/transactions/:transaction_id',
     name: 'transactionDetail',
   },
-  PhoneNumberAdd: {
-    path: '/home/settings/phone/add',
-    name: 'phoneNumberAdd',
+  PhoneNumberSetup: {
+    path: '/home/settings/phone/setup',
+    name: 'PhoneNumberSetup',
   },
   PhoneNumberVerify: {
     path: '/home/settings/phone/add/verify',
--- src/screens/setting/UserSettings.vue
+++ src/screens/setting/UserSettings.vue
@@ -7,9 +7,9 @@
           <md-divider />
           <base-setting-list-item :title="$t('message.passcode.pin_setup_title')" @click="onSetupPINClicked" />
           <md-divider />
-          <!-- <base-setting-list-item :title="$t('message.settings.phoneNumber')" @click="onSetupPhoneNumberClicked"
+          <base-setting-list-item :title="$t('message.settings.phoneNumber')" @click="onSetupPhoneNumberClicked"
             />
-            <md-divider /> -->
+            <md-divider />
           <md-divider />
           <base-setting-list-item :title="$t('message.passcode.forgot_pin')" @click="onPasscodeForgotClicked" />
           <md-divider />
@@ -52,6 +52,7 @@ export default {
       // check if the Phone Number has already set and show popup
 
       // Push
+      this.$router.push(RouteDef.PhoneNumberSetup.path);
     },
     onPasscodeForgotClicked() {
       this.$router.push(RouteDef.PinCodeForgot.path);
