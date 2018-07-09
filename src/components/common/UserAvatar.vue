7d671723be654c4abba41d6eefd0413d3c6395a5
gem <gem@mailtime.com>
2018-07-09 15:22:21 +0800

    add avatar background color

--- src/components/common/UserAvatar.vue
+++ src/components/common/UserAvatar.vue
@@ -1,5 +1,5 @@
 <template>
-  <md-avatar class="md-avatar-icon" :disabled="disabled">
+  <md-avatar class="md-avatar-icon" :disabled="disabled" :style="{ 'background-color': backgroundColor }">
     <img :src="imageSrc"
          alt="People"
          v-if="imageSrc">
@@ -16,7 +16,7 @@ export default {
   props: {
     user:
     {
-      type: String,
+      type: Object,
     },
     src: {
       type: String,
@@ -34,6 +34,14 @@ export default {
     },
     displayName() {
       return extractNameInitials(this.name || this.user.displayName || this.user.emailAddress);
+    },
+    backgroundColor() {
+      const colorCodes = ['#fad689', '#d7c4bb', '#ffba84', '#b9887d', '#b5caa0', '#a8d8b9', '#a5dee4', '#b2b2d8', '#eea9a9'];
+      const name = this.displayName.toLowerCase();
+      const firstChar = name.charAt(0);
+      let index = ((firstChar.charCodeAt(0) - 'a'.charCodeAt(0)) * colorCodes.length) / 26;
+      index = parseInt(index, 10);
+      return colorCodes[index];
     },
   },
 };
