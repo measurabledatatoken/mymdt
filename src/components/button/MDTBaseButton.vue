6c2056322b16b13f04adaba457bee5e5a864c1cd
gem <gem@mailtime.com>
2018-07-11 16:25:09 +0800

    prevent base button from accidentally double clicked.

--- src/components/button/MDTBaseButton.vue
+++ src/components/button/MDTBaseButton.vue
@@ -1,8 +1,28 @@
 <template>
-    <md-button v-bind="$attrs" @click="$emit('click')">
+    <md-button v-bind="$attrs" @click="onClick" :disabled="disabled">
         <slot></slot>
     </md-button>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      disabled: false,
+    };
+  },
+  methods: {
+    onClick() {
+      this.disabled = true;
+      this.$emit('click');
+      setTimeout(() => {
+        this.disabled = false;
+      }, 1000);
+    },
+  },
+};
+</script>
+
 
 <style lang="scss" scoped>
 .md-button.md-raised {
