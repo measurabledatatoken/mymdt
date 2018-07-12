77d6a8c49fe80f882cdc501ed3fc98aade75f7b3
gem <gem@mailtime.com>
2018-07-12 22:47:33 +0800

    refactor TransferType to enum. add transfer event tracking

--- src/api/transfer.js
+++ src/api/transfer.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { TransferType } from '@/constants';
+import TransferType from '@/enum/transferType';
 import handleGeneralResponse from './helper';
 import { APIEndPoint, APIScheme } from './constants';
 
