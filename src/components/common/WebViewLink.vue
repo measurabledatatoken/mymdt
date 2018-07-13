bea02f83f7488229ab3e2f56f5dbaaf8a382eeb3
Maxi Cho <maxi@mailtime.com>
2018-07-13 18:07:19 +0800

    1. Add Legal and Privacy screen 2. Add WebView to control the flow of viewing external url

---
+++ src/components/common/WebViewLink.vue
@@ -0,0 +1,44 @@
+<template>
+  <a @click="handleClick">
+    <slot />
+  </a>
+</template>
+
+<script>
+import { RouteDef } from '@/constants';
+
+export default {
+  props: {
+    to: {
+      type: String,
+      default: null,
+    },
+  },
+  methods: {
+    handleClick() {
+      if (!this.to) {
+        return;
+      }
+
+      this.$router.push({
+        name: RouteDef.WebView.name,
+        params: {
+          url: this.to,
+        },
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.legal-and-privacy-screen {
+  text-align: left;
+  padding: 0 $defaultPageMargin $defaultPageMargin $defaultPageMargin;
+
+  > p {
+    white-space: pre-line;
+    width: 100%;
+  }
+}
+</style>
---
+++ src/screens/WebView.vue
@@ -0,0 +1,25 @@
+<template>
+  <iframe
+    :src="url"
+    class="webview-screen"
+  />
+</template>
+
+<script>
+import BasePage from '@/screens/BasePage';
+
+export default {
+  extends: BasePage,
+  computed: {
+    url() {
+      return this.$route.params.url;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.webview-screen {
+  border: none;
+}
+</style>
