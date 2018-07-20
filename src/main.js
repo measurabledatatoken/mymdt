b85664a2814edd7efcdc45bc5256d678018788a6
Maxi Cho <maxi@mailtime.com>
2018-07-20 18:39:22 +0800

    1. implement code splitting by routes https://router.vuejs.org/guide/advanced/lazy-loading.html 2. change webpack config to workaround https://github.com/vuejs/vue-cli/issues/1669. Can remove after update vue-cli to future release 3. move Landing screen login to main.js 4. add loading indicator and error handling for async route

--- src/main.js
+++ src/main.js
@@ -9,18 +9,19 @@ import Vuelidate from 'vuelidate';
 import mixpanel from 'mixpanel-browser';
 
 // require styles
-// eslint-disable-next-line
 import 'swiper/dist/css/swiper.css';
 import VueAwesomeSwiper from 'vue-awesome-swiper';
 
 import App from './App';
 import router from './router';
-
 import store from './store';
 import i18n from './localization';
 
 import fixVirtualKeyboardGlitch from './helpers/fixVirtualKeyboardGlitch';
 import inobounce from './helpers/inobounce';
+
+import { RouteDef } from '@/constants';
+import { SET_IS_LOADING, OPEN_ERROR_PROMPT } from '@/store/modules/common';
 
 Vue.config.productionTip = false;
 Vue.use(VueMaterial);
@@ -30,14 +31,43 @@ Vue.use(VueAwesomeSwiper);
 
 mixpanel.init('c5e652bf9ccc28d929ee1c8184a0f76b');
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
+const app = new Vue({
   store,
   i18n,
   router,
-  components: { App },
-  template: '<App/>',
+  render: h => h(App),
+});
+
+const landingRoute = RouteDef.BetaTesting;
+const originalLocation = router.resolve(window.location.href);
+router.replace({
+  ...originalLocation.location,
+  ...landingRoute,
+});
+
+router.onReady(() => {
+  app.$mount('#app');
+});
+
+router.beforeEach((to, from, next) => {
+  store.commit(SET_IS_LOADING, true);
+  next();
+});
+
+router.afterEach(() => {
+  store.commit(SET_IS_LOADING, false);
+});
+
+router.onError(() => {
+  store.commit(SET_IS_LOADING, false);
+  store.dispatch(OPEN_ERROR_PROMPT, {
+    message: {
+      messageId: 'message.common.unknow_error',
+    },
+    title: {
+      messageId: 'message.common.error_title',
+    },
+  });
 });
 
 fixVirtualKeyboardGlitch();
