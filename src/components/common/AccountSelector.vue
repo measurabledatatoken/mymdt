62cce690d338f2b75fd70f5e6c082310dc53b8bd
Maxi Cho <maxi@mailtime.com>
2018-07-26 12:15:41 +0800

    fix AccountSelector 1. adjust height to display more rows 2. fix for Android 4.4 scrolling issue

--- src/components/common/AccountSelector.vue
+++ src/components/common/AccountSelector.vue
@@ -54,7 +54,7 @@
         </md-button>
 
         <!-- Content -->
-        <md-menu-content>
+        <md-menu-content :class="{ 'account-selector-menu-content--selected': selectedAccount }">
           <AccountSelectorMenuItem
             v-if="!selectedOther && selectedAccount"
             :account="selectedAccount"
@@ -344,27 +344,32 @@ $menuItemOtherCellHeight: 44px;
 .md-menu-content {
   border-radius: 0px 0px 4px 4px !important;
   background-color: $theme-listing-bgcolor;
+  max-height: 38vh;
   width: 100%;
   left: 0;
   box-shadow: 0 5px 5px -3px rgba(0, 0, 0, 0.2),
     0 0px 10px 1px rgba(0, 0, 0, 0.14), 0 3px 14px 2px rgba(0, 0, 0, 0.12);
   -webkit-overflow-scrolling: touch;
 
-  /deep/ .md-list {
-    background-color: $theme-listing-bgcolor;
-    border-radius: 0px 0px 4px 4px !important;
-    padding: 0;
+  &.account-selector-menu-content--selected {
+    max-height: 46vh;
   }
 
   /deep/ .md-menu-content-container {
     overflow: hidden;
+    display: flex;
+    flex-direction: column;
 
     .md-list {
-      height: 100%;
+      flex: 1;
+      background-color: $theme-listing-bgcolor;
+      border-radius: 0px 0px 4px 4px !important;
+      padding: 0;
 
       .account-selector-sub-menu {
         overflow: auto;
         border-top: solid 1px #eeeeee;
+        flex: 1;
 
         .account-selector-sub-menu-list {
           list-style: none;
