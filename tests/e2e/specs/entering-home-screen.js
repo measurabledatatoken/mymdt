8165696be5cd3a84634e14f8b22cafde4aad7c67
Maxi Cho <maxi@mailtime.com>
2018-08-06 17:22:40 +0800

    Beta Testing is ended. Change the test senarios.

--- tests/e2e/specs/entering-home-screen.js
+++ tests/e2e/specs/entering-home-screen.js
@@ -1,21 +1,21 @@
 describe('Entering Home Screen', () => {
-  context('with Beta Testing On', () => {
+  context('with Beta Testing screen', () => {
     beforeEach(function() {
       const deviceId = 'registeredDeviceId';
-      cy.server();
-      cy.route({
-        method: 'GET',
-        url: `/api/devices/${deviceId}/beta-testing-session`,
-        status: 200,
-        response: {
-          data: {
-            session_exists: true,
-          },
-          meta: {
-            status: 200,
-          },
-        },
-      });
+      // cy.server();
+      // cy.route({
+      //   method: 'GET',
+      //   url: `/api/devices/${deviceId}/beta-testing-session`,
+      //   status: 200,
+      //   response: {
+      //     data: {
+      //       session_exists: true,
+      //     },
+      //     meta: {
+      //       status: 200,
+      //     },
+      //   },
+      // });
       cy.wrap(deviceId).as('registeredDeviceId');
     });
 
@@ -24,14 +24,14 @@ describe('Entering Home Screen', () => {
       cy.get('.md-dialog').should('exist');
     });
 
-    it('asks for access code if non-registered device id is specified', () => {
-      cy.visit('/?deviceid=someNonRegisteredDeviceId');
+    it('show Beta Testing ending reminder screen', () => {
+      cy.visit('/?deviceid=arbiraryDeviceId');
       cy.get('.beta-testing-form__wrapper--active').should('exist');
     });
 
-    it('redirects to home screen if registered device id is specified', () => {
+    it('redirects to home screen if isadmin = true', () => {
       cy.get('@registeredDeviceId').then(registeredDeviceId => {
-        cy.visit(`/?deviceid=${registeredDeviceId}`);
+        cy.visit(`/?deviceid=${registeredDeviceId}&isadmin=true`);
         cy.location('pathname').should('eq', '/');
       });
     });
@@ -41,7 +41,7 @@ describe('Entering Home Screen', () => {
         cy.visit(
           `/autologin?appid=${Cypress.env(
             'APP_ID',
-          )}&tokens=AA3sx3rvtfedy5327smh11uppeu,a9d323flfos38tydaio6tz261,ctnxz4pbthrjob9ivu3hefzc4,exdmch39wyp64upo2g6auc1ny,9sn72u7630a31s815onxd08gx,AAcgmay7gvspdaqiq1jxy5x8o0q&emails=test001@jianxinapp.com,test002@jianxinapp.com,mailtimeai@yahoo.com,appmailtime@163.com,testmailtime001@gmail.com,gem@mailtime.com&lang=en-us&needexit=true&deviceid=${registeredDeviceId}`,
+          )}&tokens=AA3sx3rvtfedy5327smh11uppeu,a9d323flfos38tydaio6tz261,ctnxz4pbthrjob9ivu3hefzc4,exdmch39wyp64upo2g6auc1ny,9sn72u7630a31s815onxd08gx,AAcgmay7gvspdaqiq1jxy5x8o0q&emails=test001@jianxinapp.com,test002@jianxinapp.com,mailtimeai@yahoo.com,appmailtime@163.com,testmailtime001@gmail.com,gem@mailtime.com&lang=en-us&needexit=true&deviceid=${registeredDeviceId}&isadmin=true`,
         );
 
         cy.get('.bottom-content')
