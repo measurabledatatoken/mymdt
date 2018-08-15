541fb07d5baa29697456cc5cd0cbbeaf8292058e
gem <gem@mailtime.com>
2018-08-15 15:40:06 +0800

    - support read config file in fixture

--- tests/e2e/plugins/index.js
+++ tests/e2e/plugins/index.js
@@ -1,57 +1,10 @@
 // https://docs.cypress.io/guides/guides/plugins-guide.html
-
-const path = require('path');
-const fs = require('fs');
-
+const { loadConfigurationByFile } = require('./../config/config');
 const generateFixtures = require('./generateFixtures');
 
-/**
- * make config to be set environment-wise using trick described at https://docs.cypress.io/api/plugins/configuration-api.html#Promises
- * What's more, one can provide a local.json to override all configs
- * @param {string} file
- */
-function getConfigurationByFile(file) {
-  const pathToConfigFile = path.resolve(
-    __dirname,
-    '..',
-    'config',
-    `${file}.json`,
-  );
-
-  let envConfig = {};
-  try {
-    envConfig = JSON.parse(fs.readFileSync(pathToConfigFile, 'utf8'));
-  } catch (error) {
-    envConfig = {};
-  }
-
-  const pathToLocalConfigFile = path.resolve(
-    __dirname,
-    '..',
-    'config',
-    'local.json',
-  );
-
-  let localConfig = {};
-  try {
-    localConfig = JSON.parse(fs.readFileSync(pathToLocalConfigFile, 'utf8'));
-  } catch (error) {
-    localConfig = {};
-  }
-
-  const env = Object.assign({}, envConfig.env, localConfig.env);
-  return Object.assign({}, envConfig, localConfig, { env });
-}
-
 module.exports = (on, config) => {
-  // on('task', {
-  //   'defaults:fixtures': () => generateFixtures(),
-  // });
+  const envConfig = loadConfigurationByFile(config.env.configFile || 'qa');
   generateFixtures();
-
-  const envConfig = getConfigurationByFile(
-    config.env.configFile || 'production',
-  );
 
   const env = Object.assign({}, config.env, envConfig.env);
   delete env.configFile;
