d2433a87a58b77355a38dcf017401096c31bcb8a
Matt Lung <matt@mailtime.com>
2018-08-20 18:18:17 +0800

    Updated e2e testing for setup pin and phone

--- tests/e2e/specs/settings/setup-phone.js
+++ tests/e2e/specs/settings/setup-phone.js
@@ -1,10 +1,14 @@
 import createAPIResponse from '../../utils/createAPIResponse';
 
-describe('Entering Setting Page', () => {
+describe('Setup Phone', () => {
   beforeEach(() => {
-    cy.server();
+    cy.clearLocalStorage();
     cy.stubUserListingAndDetail('user/passcodeset');
     cy.stubPinVerify();
+    cy.login();
+    cy.goToSettingPage();
+    cy.goToUserSettingPage();
+
     cy.route('POST', '/api/security/sms/requestotp', createAPIResponse([])).as(
       'requestotp',
     );
@@ -12,61 +16,13 @@ describe('Entering Setting Page', () => {
       'addPhoneNumber',
     );
   });
-  const inputMobileForSetup = (countryCode = '+852', phone = '61111111') => {
-    cy.getCurrentContentRouterView()
-      .find('input[type="number"]')
-      .then($input => {
-        cy.wrap($input).type(phone);
-      });
-
-    cy.getCurrentContentRouterView()
-      .find('input[type="text"]')
-      .click();
-    cy.get('.dial-code')
-      .contains(countryCode)
-      .parents('.md-list-item-button')
-      .click();
-  };
-  const SetupPhoneNumber = () => {
-    const rowIndexInUserSettingOptions = 1;
-
-    cy.getCurrentContentRouterView()
-      .find('.md-list>.md-list-item')
-      .eq(rowIndexInUserSettingOptions)
-      .click();
-
-    cy.inputPinCode();
-    // setup pin page
-    cy.location('pathname').should('eq', '/home/settings/phone/add');
-
-    inputMobileForSetup();
-
-    cy.getCurrentContentRouterView()
-      .find('button:contains("Next")')
-      .click();
-
-    cy.wait('@requestotp');
-    cy.inputSMSVerificationCode();
-
-    // add phone success popup
-    cy.get('.md-dialog')
-      .find('button:contains("Done")')
-      .click();
-
-    cy.getCurrentContentRouterView()
-      .find('.md-list>.md-list-item')
-      .eq(rowIndexInUserSettingOptions)
-      .find('.icon-complete')
-      .should('exist');
-  };
-  it('can enter setting page', () => {
-    cy.login();
-    cy.goToSettingPage();
-  });
-  it('can enter user setting page', () => {
-    cy.goToUserSettingPage();
-  });
   it('can setup phone number', () => {
-    SetupPhoneNumber();
+    cy.get('[data-cy="setting-setup-phone"]').click();
+    cy.inputPinCode();
+    cy.addPhoneNumber();
+
+    cy.get('[data-cy="setting-setup-phone"]')
+      .find('[data-cy="icon-complete"]')
+      .should('exist');
   });
 });
--- tests/e2e/specs/settings/setup-pin.js
+++ tests/e2e/specs/settings/setup-pin.js
@@ -1,73 +1,19 @@
-import createAPIResponse from '../../utils/createAPIResponse';
-
-describe('Entering Setting Page', () => {
+describe('Setup PIN', () => {
   beforeEach(() => {
-    cy.server();
-
+    cy.clearLocalStorage();
     cy.stubUserListingAndDetail('user/index');
     cy.stubPinVerify();
-    //setup pin api
-    cy.route('POST', '/api/security/pin/setup', createAPIResponse([])).as(
-      'setupPin',
-    );
-
-    cy.route('POST', '/api/security/sms/requestotp', createAPIResponse([])).as(
-      'requestotp',
-    );
-
-    cy.route('POST', '/api/security/phonenumber/add', createAPIResponse([])).as(
-      'addPhoneNumber',
-    );
+    cy.login();
+    cy.goToSettingPage();
+    cy.goToUserSettingPage();
   });
 
-  const goToSettingPage = () => {
-    cy.login();
-    cy.get('.settingsbtn').click();
-    cy.fixture('user/users').then(users => {
-      users.forEach(user => {
-        cy.get('.md-list.md-double-line').contains(user.email_address);
-      });
-    });
-  };
-  const goToUserSettingPage = () => {
-    cy.get('@selectedUserEmail').then(selectedUserEmail => {
-      cy.get('.md-list.md-double-line')
-        .contains(selectedUserEmail)
-        .parents('.md-list-item')
-        .click();
-      cy.location('pathname').should('eq', '/home/usersettings');
-    });
-  };
-  const SetupPIN = (pin = '111111') => {
-    cy.getCurrentContentRouterView()
-      .find('.md-list>.md-list-item')
-      .eq(0)
-      .click();
-    // setup pin page
-    cy.location('pathname').should('eq', '/home/settings/pincode/setup');
-
-    inputPINForSetup(pin);
-    cy.getCurrentContentRouterView()
-      .find('button:contains("Next")')
-      .click();
-
-    // setup pin confirm page
-    cy.location('pathname').should('eq', '/home/settings/pincode/confirm');
-    inputPINForSetup(pin);
-    cy.getCurrentContentRouterView()
-      .find('button:contains("Next")')
-      .click();
-
-    cy.wait('@setupPin');
-
-    // setup pin success popup
-    cy.get('.md-dialog')
-      .find('button:contains("Done")')
-      .click();
-
+  it('can setup pin', () => {
+    cy.get('[data-cy="setting-setup-pin"]').click();
+    cy.setupPIN();
     // Add your phone number page
     cy.getCurrentContentRouterView()
-      .find('button:contains("Skip")')
+      .find('[data-cy="skip"]')
       .click();
 
     // skip adding phone number popup
@@ -78,31 +24,22 @@ describe('Entering Setting Page', () => {
       .click();
 
     // check if the setup pin indicator icon exists
-    cy.getCurrentContentRouterView()
-      .find('.md-list>.md-list-item')
-      .eq(0)
-      .find('.icon-complete')
+    cy.get('[data-cy="setting-setup-pin"]')
+      .find('[data-cy="icon-complete"]')
       .should('exist');
-  };
-  const inputPINForSetup = (pin = '111111') => {
-    expect(pin).to.have.lengthOf(6);
-    cy.wait(750);
-    cy.getCurrentContentRouterView()
-      .find('.pin-code-field')
-      .find('input')
-      .then($input => {
-        Cypress._.each($input, (el, index) => {
-          cy.wrap(el).type(pin[index]);
-        });
-      });
-  };
-  it('can enter setting page', () => {
-    goToSettingPage();
   });
-  it('can enter user setting page', () => {
-    goToUserSettingPage();
-  });
-  it('can setup pin', () => {
-    SetupPIN('111111');
+  it('can setup pin and phone', () => {
+    cy.get('[data-cy="setting-setup-pin"]').click();
+    cy.setupPIN();
+    cy.addPhoneNumber();
+
+    // check if the setup pin indicator icon exists
+    cy.get('[data-cy="setting-setup-pin"]')
+      .find('[data-cy="icon-complete"]')
+      .should('exist');
+
+    cy.get('[data-cy="setting-setup-phone"]')
+      .find('[data-cy="icon-complete"]')
+      .should('exist');
   });
 });
