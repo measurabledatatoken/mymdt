3ed074c964b70448f77c215f4bdad6004ba7cb21
Matt Lung <matt@mailtime.com>
2018-08-23 15:25:26 +0800

    Added maintenance mode handling
    Added e2e testing for maintenance mode

--- src/screens/WelcomePage.vue
+++ src/screens/WelcomePage.vue
@@ -9,7 +9,12 @@
       </div>
       <div class="welcome-form__content">
         <p 
-          v-if="isOSSupported" 
+          v-if="inMaintenance"
+          data-cy="maintenance-message" 
+          v-html="$t('message.maintenance.description')"
+        />
+        <p 
+          v-else-if="isOSSupported" 
           v-html="$t('message.welcome.description')"
         />
         <p 
@@ -18,7 +23,7 @@
         />
       </div>
       <div 
-        v-if="isOSSupported" 
+        v-if="isAvailable" 
         class="welcome-form__footer"
       >
         <Checkbox 
@@ -60,7 +65,7 @@ import NDA from '@/components/betaTesting/NDA';
 import { OPEN_ERROR_PROMPT } from '@/store/modules/common';
 
 import { RouteDef } from '@/constants';
-import { SET_NDA_AGREEMENT } from '@/store/modules/home';
+import { SET_NDA_AGREEMENT, REQUEST_APP_CONFIG } from '@/store/modules/home';
 
 const checked = value => !helpers.req(value) || value === true;
 
@@ -76,6 +81,7 @@ export default {
       agree: false,
       showScreen: false,
       showNDA: false,
+      inMaintenance: false,
     };
   },
   validations: {
@@ -86,7 +92,11 @@ export default {
   computed: {
     ...mapState({
       ndaAgreement: state => state.home.ndaAgreement,
+      appConfig: state => state.home.appConfig,
     }),
+    isAvailable() {
+      return this.isAdmin || (this.isOSSupported && !this.inMaintenance);
+    },
     isOSSupported() {
       // return false;
       var ua = navigator.userAgent;
@@ -102,10 +112,14 @@ export default {
         return true;
       }
     },
+    isAdmin() {
+      return this.$route.query.isadmin;
+    },
   },
-  created() {
-    const isAdmin = this.$route.query.isadmin;
-    if ((isAdmin || this.ndaAgreement) && this.isOSSupported) {
+  async created() {
+    await this.requestAppConfig();
+    this.inMaintenance = this.appConfig.server_status !== 'active';
+    if ((this.isAvailable && this.ndaAgreement) || this.isAdmin) {
       this.goToHome();
     } else {
       this.showScreen = true;
@@ -118,6 +132,7 @@ export default {
     }),
     ...mapActions({
       openErrorPrompt: OPEN_ERROR_PROMPT,
+      requestAppConfig: REQUEST_APP_CONFIG,
     }),
     goToHome() {
       this.$router.replace({
