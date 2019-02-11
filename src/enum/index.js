879b0f56c10fcfa9f6ef9598b2d11f756f53507f
Matt Lung <matt@mailtime.com>
2019-02-11 18:52:20 +0800

    Updated error codes clickable area of Reward with data point is changed to whole row. Updated data point keys of reward. Updated translations.

--- src/enum/index.js
+++ src/enum/index.js
@@ -1,3 +1,4 @@
 export { default as transactionType } from './transactionType';
 export { default as transactionStatus } from './transactionStatus';
+export { default as rewardDataPointType } from './rewardDataPointType';
 export { default as ErrorCode } from './errorCode';
