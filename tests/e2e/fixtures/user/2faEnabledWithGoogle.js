39cbe12a404ac37f0076a5dc6b021d85632051ab
Matt Lung <matt@mailtime.com>
2019-04-01 15:33:44 +0800

    Need to setup phone number when entering transfer email/eth page Updated related e2e tests

--- tests/e2e/fixtures/user/2faEnabledWithGoogle.js
+++ tests/e2e/fixtures/user/2faEnabledWithGoogle.js
@@ -2,6 +2,7 @@ const user = require('./index');
 
 module.exports = Object.assign({}, user, {
   is_passcode_set: true,
+  is_phone_confirmed: true,
   is_2fa_enabled: true,
   '2fa_method': 2,
   '2fa_usage': 3,
