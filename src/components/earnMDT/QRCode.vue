ae6b0bc52c9f65aa038e34b651f26879e3f15d10
Maxi Cho <maxi@mailtime.com>
2019-07-09 11:42:34 +0800

    add QR code component for WeChat following

---
+++ src/components/earnMDT/QRCode.vue
@@ -0,0 +1,29 @@
+<template>
+  <VueQrcode 
+    :value="value"
+    tag="img"
+  />
+</template>
+
+<script>
+import VueQrcode from '@chenfengyuan/vue-qrcode';
+
+export default {
+  components: {
+    VueQrcode,
+  },
+  props: {
+    value: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+
+
+<style lang="scss" scoped>
+img {
+  -webkit-touch-callout: default; // enable Save Image for iOS WebView which is disabled by vue-material
+}
+</style>
