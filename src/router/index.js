1cbf41f43908714734d3e84e90a9b2aacefa02ee
Cho Chi Him <chochihim@gmail.com>
2019-09-11 15:48:56 +0800

    add feature flag VUE_APP_FEATURE_TRANSFER_BY_EMAIL for "transfer by email" feature toggling

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Meta from 'vue-meta';
-
 import { RouteDef } from '@/constants';
 
 import AppLayout from '@/container/AppLayout';
+
+import { enableTransferByEmail } from '@/constants';
 
 const Home = () => import('@/screens/Home');
 const Welcome = () => import('@/screens/WelcomePage');
@@ -83,232 +84,237 @@ import SimpleHeader from '@/components/header/SimpleHeader';
 Vue.use(Router);
 Vue.use(Meta);
 
+const transferRoutes = [
+  {
+    path: RouteDef.TransferList.path,
+    name: RouteDef.TransferList.name,
+    component: TransferList,
+  },
+  {
+    path: RouteDef.TransferEthWallet.path,
+    component: TransferEthWallet,
+  },
+  {
+    path: RouteDef.TransferReview.path,
+    component: TransferReview,
+  },
+  {
+    path: RouteDef.TransferEthWalletQrCode.path,
+    component: QrCodeScanPage,
+  },
+  {
+    path: RouteDef.TransferSuccess.path,
+    name: RouteDef.TransferSuccess.name,
+    component: TransferSuccess,
+    props: true,
+  },
+  {
+    path: RouteDef.TransferVerifyGoogleAuthPage.path,
+    name: RouteDef.TransferVerifyGoogleAuthPage.name,
+    component: TransferVerifyGoogleAuthPage,
+    props: true,
+  },
+];
+
+if (enableTransferByEmail) {
+  transferRoutes.push({
+    path: RouteDef.TransferEmail.path,
+    component: TransferEmail,
+  });
+}
+
+const subRoutes = [
+  {
+    path: RouteDef.Home.path,
+    component: Home,
+  },
+  {
+    path: RouteDef.Login.path,
+    component: Login,
+  },
+  {
+    path: RouteDef.AutoLogin.path,
+    redirect: RouteDef.Home,
+  },
+  {
+    path: RouteDef.Welcome.path,
+    components: {
+      default: Welcome,
+      header: SimpleHeader,
+    },
+    meta: {
+      setFixHeight: false,
+    },
+  },
+  {
+    path: RouteDef.Register.path,
+    component: Register,
+  },
+  {
+    path: RouteDef.ForgetPassword.path,
+    component: ForgetPassword,
+  },
+  {
+    path: RouteDef.Settings.path,
+    component: Settings,
+  },
+  {
+    path: RouteDef.DataPointRewardListing.path,
+    name: RouteDef.DataPointRewardListing.name,
+    component: DataPointRewardListing,
+  },
+  {
+    path: RouteDef.DataPointRewardDetail.path,
+    name: RouteDef.DataPointRewardDetail.name,
+    component: DataPointRewardDetail,
+    props: true,
+  },
+  {
+    path: RouteDef.DataPointRewardDetailNew.path,
+    name: RouteDef.DataPointRewardDetailNew.name,
+    component: DataPointRewardDetailNew,
+    props: true,
+  },
+  {
+    path: RouteDef.UserSettings.path,
+    name: RouteDef.UserSettings.name,
+    component: UserSettings,
+    props: true,
+  },
+  // phone
+  {
+    path: RouteDef.PhoneNumberInput.path,
+    name: RouteDef.PhoneNumberInput.name,
+    component: PhoneNumberInputPage,
+    props: true,
+  },
+  {
+    path: RouteDef.PhoneNumberVerify.path,
+    name: RouteDef.PhoneNumberVerify.name,
+    component: PhoneNumberVerifyPage,
+    props: true,
+  },
+  // Pin Code
+  {
+    path: RouteDef.PinCodeSetup.path,
+    name: RouteDef.PinCodeSetup.name,
+    component: PinCodeSetup,
+    props: true,
+  },
+  {
+    path: RouteDef.PinCodeConfirm.path,
+    name: RouteDef.PinCodeConfirm.name,
+    component: PinCodeConfirm,
+    props: true,
+  },
+  {
+    path: RouteDef.PinCodeForgot.path,
+    name: RouteDef.PinCodeForgot.name,
+    component: PinCodeForgot,
+    props: true,
+  },
+  {
+    path: RouteDef.PriceUnits.path,
+    component: PriceUnits,
+  },
+  {
+    path: RouteDef.ReportProblem.path,
+    component: ReportProblem,
+  },
+  {
+    path: RouteDef.ReportProblemSuccess.path,
+    component: ReportProblemSuccess,
+  },
+  {
+    path: RouteDef.LegalAndPrivacy.path,
+    component: LegalAndPrivacy,
+  },
+  {
+    path: RouteDef.Tutorial.path,
+    component: Tutorial,
+  },
+  {
+    path: RouteDef.AccountDetail.path,
+    name: RouteDef.AccountDetail.name,
+    component: AccountDetail,
+  },
+  {
+    path: RouteDef.TransactionDetail.path,
+    name: RouteDef.TransactionDetail.name,
+    component: TransactionDetail,
+    props: true,
+  },
+  ...transferRoutes,
+  // Earn MDT Route
+  {
+    path: RouteDef.EarnMDT.path,
+    name: RouteDef.EarnMDT.name,
+    component: EarnMDT,
+  },
+  {
+    path: RouteDef.FollowSocialMedia.path,
+    name: RouteDef.FollowSocialMedia.name,
+    component: FollowSocialMedia,
+  },
+
+  {
+    path: RouteDef.WebView.path,
+    name: RouteDef.WebView.name,
+    component: WebView,
+  },
+
+  // Google Authenticator Setting Page Route
+  {
+    path: RouteDef.GoogleAuthSettingStep1.path,
+    name: RouteDef.GoogleAuthSettingStep1.name,
+    component: GoogleAuthSettingStep1,
+  },
+  {
+    path: RouteDef.GoogleAuthSettingStep2.path,
+    name: RouteDef.GoogleAuthSettingStep2.name,
+    component: GoogleAuthSettingStep2,
+  },
+  {
+    path: RouteDef.GoogleAuthSettingStep3.path,
+    name: RouteDef.GoogleAuthSettingStep3.name,
+    component: GoogleAuthSettingStep3,
+  },
+  {
+    path: RouteDef.DisableGoogleAuthVerifyPage.path,
+    name: RouteDef.DisableGoogleAuthVerifyPage.name,
+    component: DisableGoogleAuthVerifyPage,
+  },
+  // Two Factor Setting Page Route
+  {
+    path: RouteDef.TwoFactorAuthenticationSetting.path,
+    name: RouteDef.TwoFactorAuthenticationSetting.name,
+    component: TwoFactorAuthenticationSettingPage,
+    props: true,
+  },
+  {
+    path: RouteDef.DisableTwoFactorAuthenticationVerifyGoogleAuthPage.path,
+    name: RouteDef.DisableTwoFactorAuthenticationVerifyGoogleAuthPage.name,
+    component: DisableTwoFactorAuthenticationVerifyGoogleAuthPage,
+  },
+  {
+    path: RouteDef.ETHBinding.path,
+    name: RouteDef.ETHBinding.name,
+    component: ETHBinding,
+  },
+  // Route to Home page for route not defined
+  {
+    path: '*',
+    redirect: RouteDef.Home,
+  },
+];
+
 const router = new Router({
   mode: 'history',
   routes: [
     {
       path: '',
       component: AppLayout,
-      children: [
-        {
-          path: RouteDef.Home.path,
-          component: Home,
-        },
-        {
-          path: RouteDef.Login.path,
-          component: Login,
-        },
-        {
-          path: RouteDef.AutoLogin.path,
-          redirect: RouteDef.Home,
-        },
-        {
-          path: RouteDef.Welcome.path,
-          components: {
-            default: Welcome,
-            header: SimpleHeader,
-          },
-          meta: {
-            setFixHeight: false,
-          },
-        },
-        {
-          path: RouteDef.Register.path,
-          component: Register,
-        },
-        {
-          path: RouteDef.ForgetPassword.path,
-          component: ForgetPassword,
-        },
-        {
-          path: RouteDef.Settings.path,
-          component: Settings,
-        },
-        {
-          path: RouteDef.DataPointRewardListing.path,
-          name: RouteDef.DataPointRewardListing.name,
-          component: DataPointRewardListing,
-        },
-        {
-          path: RouteDef.DataPointRewardDetail.path,
-          name: RouteDef.DataPointRewardDetail.name,
-          component: DataPointRewardDetail,
-          props: true,
-        },
-        {
-          path: RouteDef.DataPointRewardDetailNew.path,
-          name: RouteDef.DataPointRewardDetailNew.name,
-          component: DataPointRewardDetailNew,
-          props: true,
-        },
-        {
-          path: RouteDef.UserSettings.path,
-          name: RouteDef.UserSettings.name,
-          component: UserSettings,
-          props: true,
-        },
-        // phone
-        {
-          path: RouteDef.PhoneNumberInput.path,
-          name: RouteDef.PhoneNumberInput.name,
-          component: PhoneNumberInputPage,
-          props: true,
-        },
-        {
-          path: RouteDef.PhoneNumberVerify.path,
-          name: RouteDef.PhoneNumberVerify.name,
-          component: PhoneNumberVerifyPage,
-          props: true,
-        },
-        // Pin Code
-        {
-          path: RouteDef.PinCodeSetup.path,
-          name: RouteDef.PinCodeSetup.name,
-          component: PinCodeSetup,
-          props: true,
-        },
-        {
-          path: RouteDef.PinCodeConfirm.path,
-          name: RouteDef.PinCodeConfirm.name,
-          component: PinCodeConfirm,
-          props: true,
-        },
-        {
-          path: RouteDef.PinCodeForgot.path,
-          name: RouteDef.PinCodeForgot.name,
-          component: PinCodeForgot,
-          props: true,
-        },
-        {
-          path: RouteDef.PriceUnits.path,
-          component: PriceUnits,
-        },
-        {
-          path: RouteDef.ReportProblem.path,
-          component: ReportProblem,
-        },
-        {
-          path: RouteDef.ReportProblemSuccess.path,
-          component: ReportProblemSuccess,
-        },
-        {
-          path: RouteDef.LegalAndPrivacy.path,
-          component: LegalAndPrivacy,
-        },
-        {
-          path: RouteDef.Tutorial.path,
-          component: Tutorial,
-        },
-        {
-          path: RouteDef.AccountDetail.path,
-          name: RouteDef.AccountDetail.name,
-          component: AccountDetail,
-        },
-        {
-          path: RouteDef.TransactionDetail.path,
-          name: RouteDef.TransactionDetail.name,
-          component: TransactionDetail,
-          props: true,
-        },
-
-        // Transfer Route
-        {
-          path: RouteDef.TransferList.path,
-          name: RouteDef.TransferList.name,
-          component: TransferList,
-        },
-        {
-          path: RouteDef.TransferEmail.path,
-          component: TransferEmail,
-        },
-        {
-          path: RouteDef.TransferEthWallet.path,
-          component: TransferEthWallet,
-        },
-        {
-          path: RouteDef.TransferReview.path,
-          component: TransferReview,
-        },
-        {
-          path: RouteDef.TransferEthWalletQrCode.path,
-          component: QrCodeScanPage,
-        },
-        {
-          path: RouteDef.TransferSuccess.path,
-          name: RouteDef.TransferSuccess.name,
-          component: TransferSuccess,
-          props: true,
-        },
-        {
-          path: RouteDef.TransferVerifyGoogleAuthPage.path,
-          name: RouteDef.TransferVerifyGoogleAuthPage.name,
-          component: TransferVerifyGoogleAuthPage,
-          props: true,
-        },
-        // Earn MDT Route
-        {
-          path: RouteDef.EarnMDT.path,
-          name: RouteDef.EarnMDT.name,
-          component: EarnMDT,
-        },
-        {
-          path: RouteDef.FollowSocialMedia.path,
-          name: RouteDef.FollowSocialMedia.name,
-          component: FollowSocialMedia,
-        },
-
-        {
-          path: RouteDef.WebView.path,
-          name: RouteDef.WebView.name,
-          component: WebView,
-        },
-
-        // Google Authenticator Setting Page Route
-        {
-          path: RouteDef.GoogleAuthSettingStep1.path,
-          name: RouteDef.GoogleAuthSettingStep1.name,
-          component: GoogleAuthSettingStep1,
-        },
-        {
-          path: RouteDef.GoogleAuthSettingStep2.path,
-          name: RouteDef.GoogleAuthSettingStep2.name,
-          component: GoogleAuthSettingStep2,
-        },
-        {
-          path: RouteDef.GoogleAuthSettingStep3.path,
-          name: RouteDef.GoogleAuthSettingStep3.name,
-          component: GoogleAuthSettingStep3,
-        },
-        {
-          path: RouteDef.DisableGoogleAuthVerifyPage.path,
-          name: RouteDef.DisableGoogleAuthVerifyPage.name,
-          component: DisableGoogleAuthVerifyPage,
-        },
-        // Two Factor Setting Page Route
-        {
-          path: RouteDef.TwoFactorAuthenticationSetting.path,
-          name: RouteDef.TwoFactorAuthenticationSetting.name,
-          component: TwoFactorAuthenticationSettingPage,
-          props: true,
-        },
-        {
-          path:
-            RouteDef.DisableTwoFactorAuthenticationVerifyGoogleAuthPage.path,
-          name:
-            RouteDef.DisableTwoFactorAuthenticationVerifyGoogleAuthPage.name,
-          component: DisableTwoFactorAuthenticationVerifyGoogleAuthPage,
-        },
-        {
-          path: RouteDef.ETHBinding.path,
-          name: RouteDef.ETHBinding.name,
-          component: ETHBinding,
-        },
-        // Route to Home page for route not defined
-        {
-          path: '*',
-          redirect: RouteDef.Home,
-        },
-      ],
+      children: subRoutes,
     },
   ],
 });
