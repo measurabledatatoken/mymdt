2a516ef3904d245a4c7c09c6a1c545f7595a6e2c
Cho Chi Him <chochihim@gmail.com>
2019-10-15 15:58:18 +0800

    fix issues #1 #66

---
+++ src/components/button/ClaimButton.vue
@@ -0,0 +1,438 @@
+<template>
+  <div :class="['claim-button-container', { 'align-items-flex-end': expired }]">
+    <transition :name="centered ? 'fade' : 'collapse'">
+      <span 
+        v-if="showExpiryDate"
+        class="expiry-date placeholder"
+      >
+        {{ expiryText }}
+      </span>
+    </transition>
+    <div class="earn-mdt-reward-item-action">
+      <div class="earn-mdt-reward-item-action__amount-placholder">
+        <transition  
+          name="earn-mdt-reward-item-action__claim-button"    
+          @after-leave="afterLeave" 
+        >
+          <MDTSecondaryButton
+            v-show="canBeClaimed"
+            ref="claimButton"
+            :style="claimButtonStyle"
+            class="earn-mdt-reward-item-action__claim-button"
+            color="secondary"
+            v-on="$listeners"
+          >
+            {{ buttonText }}
+          </MDTSecondaryButton>
+        </transition>
+        <div class="earn-mdt-reward-item-action__amount-wrapper">
+          <transition name="earn-mdt-reward-item-action__amount">
+            <MDTSecondaryButton 
+              v-if="showAmount" 
+              :class="['earn-mdt-reward-item-action__amount', { 'amount-grey': !claimed }]" 
+              disabled
+            >{{ amountText }}</MDTSecondaryButton>
+          </transition>
+          <transition 
+            v-for="n in 8" 
+            :key="n" 
+            name="deco"
+          >    
+            <span  
+              v-if="showAmount" 
+              :class="['deco', 'deco-' + n]"
+            />
+          </transition> 
+        </div>
+      </div>
+    </div>
+    <transition :name="centered ? 'fade' : 'collapse'">
+      <span 
+        v-if="showExpiryDate"
+        class="expiry-date"
+      >
+        {{ expiryText }}
+      </span>
+    </transition>
+  </div>
+</template>
+
+<script>
+import MDTSecondaryButton from '@/components/button/MDTSecondaryButton';
+
+import { formatAmount } from '@/utils';
+import RewardStatus from '@/enum/rewardStatus';
+
+export default {
+  components: {
+    MDTSecondaryButton,
+  },
+  props: {
+    amount: {
+      type: Number,
+      default: 0,
+    },
+    currency: {
+      type: String,
+      default: 'MDT',
+    },
+    rewardStatus: {
+      type: Number,
+      default: 0,
+    },
+    expiredInDays: {
+      type: Number,
+      default: 0,
+    },
+    expiryTime: {
+      type: String || null,
+      default: '',
+    },
+    centered: {
+      type: Boolean,
+      default: false,
+    },
+    noReward: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      claimButtonStyle: null,
+      showAmount: this.rewardStatus !== RewardStatus.ACTIVE || this.noReward,
+    };
+  },
+  computed: {
+    buttonText() {
+      return `${this.$t('message.earnMDT.claim')} ${this.amountText}`;
+    },
+    amountText() {
+      return formatAmount(this.amount, {
+        prefix: this.claimed ? '+' : '',
+        suffix: ` ${this.currency}`,
+      });
+    },
+    claimed() {
+      if (this.noReward) {
+        return false;
+      }
+
+      return this.rewardStatus === RewardStatus.CLAIMED;
+    },
+    canBeClaimed() {
+      if (this.noReward) {
+        return false;
+      }
+
+      return this.rewardStatus === RewardStatus.ACTIVE;
+    },
+    expired() {
+      return this.rewardStatus === RewardStatus.EXPIRED;
+    },
+    expiryText() {
+      switch (this.rewardStatus) {
+        case RewardStatus.ACTIVE: {
+          return this.$t('message.earnMDT.expiredAfter', {
+            number: this.expiredInDays >= 0 ? this.expiredInDays : 0,
+          });
+        }
+        case RewardStatus.SUSPENDED:
+        case RewardStatus.CLAIMED: {
+          return '';
+        }
+        case RewardStatus.EXPIRED: {
+          if (this.expiryTime) {
+            return this.$t('message.earnMDT.expiredOn', {
+              date: this.$d(new Date(this.expiryTime), 'short'),
+            });
+          }
+        }
+      }
+
+      return this.$t('message.earnMDT.expired');
+    },
+    showExpiryDate() {
+      return this.canBeClaimed || this.expired;
+    },
+  },
+  mounted() {
+    this.$nextTick(() => {
+      if (this.$refs.claimButton) {
+        this.claimButtonStyle = {
+          height: `${this.$refs.claimButton.$el.clientHeight}px`,
+          width: `${this.$refs.claimButton.$el.clientWidth}px`,
+        };
+      }
+    });
+
+    // TODO: is more fine-tuned needed?
+    // const style = this.$refs.claimButton.$el.currentStyle || window.getComputedStyle(this.$refs.claimButton.$el);
+    // const marginVertical = parseInt(style.marginTop, 10) + parseInt(style.marginRight, 10);
+    // const marginHorizontal = parseInt(style.marginLeft, 10) + parseInt(style.marginRight, 10);
+
+    // this.animationPlaceholderStyle = {
+    //   height: `${this.$refs.claimButton.$el.clientHeight + marginVertical}px`,
+    //   width: `${this.$refs.claimButton.$el.clientWidth + marginHorizontal}px`,
+    // };
+  },
+  methods: {
+    afterLeave() {
+      this.showAmount = true;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.claim-button-container {
+  display: inline-flex;
+  flex-direction: column;
+  align-items: center;
+
+  &.align-items-flex-end {
+    align-items: flex-end;
+  }
+}
+
+.expiry-date {
+  color: $theme-placeholder-color;
+  font-size: 0.625rem;
+  line-height: normal;
+  margin: 0.125rem 0;
+  overflow: hidden;
+  height: 12px;
+
+  &.placeholder {
+    opacity: 0;
+  }
+}
+
+.earn-mdt-reward-item-action {
+  justify-content: flex-end;
+  display: flex;
+
+  .earn-mdt-reward-item-action__claim-button {
+    margin: 0px;
+  }
+
+  .md-button.earn-mdt-reward-item-action__claim-button-leave-active {
+    min-width: 0;
+    animation-name: shrink;
+    animation-duration: 0.4s;
+    animation-timing-function: linear;
+    animation-fill-mode: forwards;
+  }
+
+  .earn-mdt-reward-item-action__amount-to-earn {
+    font-size: 0.875rem;
+    font-weight: bold;
+    color: $theme-secondary-color;
+    margin-right: 0.625rem;
+  }
+
+  .earn-mdt-reward-item-action__amount-to-earn-leave-active {
+    animation-name: earn-mdt-reward-item-action__amount-to-earn-animation;
+    animation-duration: 0.4s;
+    animation-timing-function: linear;
+    animation-fill-mode: forwards;
+  }
+
+  .earn-mdt-reward-item-action__amount-placholder {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: auto;
+    line-height: 1;
+
+    .earn-mdt-reward-item-action__amount-wrapper {
+      position: relative;
+
+      .earn-mdt-reward-item-action__amount {
+        background-color: transparent;
+        color: $theme-secondary-color;
+        min-width: 0;
+        font-weight: bold;
+        box-shadow: none;
+        margin: 0;
+        font-size: 1rem;
+        line-height: 1;
+        padding: 0;
+
+        /deep/ .md-ripple {
+          padding: 0;
+        }
+
+        &.amount-grey {
+          color: $theme-disable-color-font;
+        }
+      }
+
+      .earn-mdt-reward-item-action__amount-enter-active {
+        animation-name: zoomIn;
+        animation-duration: 0.5s;
+        animation-fill-mode: forwards;
+      }
+
+      .deco-enter-active {
+        animation-duration: 650ms;
+        animation-delay: 150ms;
+        animation-fill-mode: forwards;
+
+        $positionLeftOfCenterPoint: 75%;
+        $positionTopOfCenterPoint: 50%;
+        $horizontalOffset: 8%;
+        &.deco-1 {
+          animation-name: vanish, moveToTop;
+          top: ($positionTopOfCenterPoint - 30%);
+          left: $positionLeftOfCenterPoint;
+          transform: rotate(90deg);
+          transform-origin: left;
+        }
+
+        &.deco-2 {
+          top: ($positionTopOfCenterPoint - 20%);
+          right: (100% - $positionLeftOfCenterPoint) - ($horizontalOffset/2);
+          animation-name: vanish, moveToTop, moveToRight;
+          transform: rotate(-45deg);
+          transform-origin: right top;
+        }
+
+        &.deco-3 {
+          top: $positionTopOfCenterPoint;
+          right: (100% - $positionLeftOfCenterPoint) - $horizontalOffset;
+          animation-name: vanish, moveToRight;
+          // transform: translateY(-50%);
+        }
+
+        &.deco-4 {
+          top: ($positionTopOfCenterPoint + 20%);
+          right: (100% - $positionLeftOfCenterPoint) - ($horizontalOffset/2);
+          transform: rotate(45deg);
+          animation-name: vanish, moveToBottom, moveToRight;
+          transform-origin: right bottom;
+        }
+
+        &.deco-5 {
+          top: ($positionTopOfCenterPoint + 30%);
+          left: $positionLeftOfCenterPoint;
+          transform: rotate(-90deg);
+          animation-name: vanish, moveToBottom;
+          transform-origin: left;
+        }
+
+        &.deco-6 {
+          top: ($positionTopOfCenterPoint + 20%);
+          left: ($positionLeftOfCenterPoint - $horizontalOffset/2);
+          transform: rotate(-45deg);
+          transform-origin: left bottom;
+          animation-name: vanish, moveToBottom, moveToLeft;
+        }
+
+        &.deco-7 {
+          top: $positionTopOfCenterPoint;
+          left: $positionLeftOfCenterPoint - $horizontalOffset;
+          animation-name: vanish, moveToLeft;
+        }
+
+        &.deco-8 {
+          top: ($positionTopOfCenterPoint - 20%);
+          left: ($positionLeftOfCenterPoint - $horizontalOffset/2);
+          transform: rotate(45deg);
+          transform-origin: left top;
+          animation-name: vanish, moveToTop, moveToLeft;
+        }
+      }
+
+      .deco {
+        height: 1px;
+        width: 0px;
+        background-color: $theme-secondary-color;
+        position: absolute;
+      }
+    }
+  }
+}
+
+.collapse-enter-active,
+.collapse-leave-active {
+  transition: height 0.4s;
+}
+.collapse-enter,
+.collapse-leave-to {
+  height: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.4s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+@keyframes shrink {
+  65% {
+    width: 24px;
+    transform: scale3d(1, 1, 1);
+  }
+  to {
+    width: 24px;
+    transform: scale3d(0, 0, 0);
+  }
+}
+
+@keyframes earn-mdt-reward-item-action__amount-to-earn-animation {
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+  }
+}
+
+@keyframes vanish {
+  from {
+    width: 8px;
+  }
+  to {
+    width: 0px;
+  }
+}
+@keyframes moveToTop {
+  to {
+    margin-top: -10px;
+  }
+}
+@keyframes moveToRight {
+  to {
+    margin-right: -10px;
+  }
+}
+@keyframes moveToBottom {
+  to {
+    margin-top: 10px;
+  }
+}
+@keyframes moveToLeft {
+  to {
+    margin-left: -10px;
+  }
+}
+@keyframes zoomIn {
+  from {
+    opacity: 0;
+    transform: scale3d(0.3, 0.3, 0.3);
+  }
+  50% {
+    opacity: 1;
+  }
+  75% {
+    transform: scale3d(1.1, 1.1, 1.1);
+  }
+  100% {
+    transform: scale3d(1, 1, 1);
+  }
+}
+</style>
--- src/components/task/RewardItem.vue
+++ src/components/task/RewardItem.vue
@@ -8,43 +8,15 @@
     md-src="/static/icons/done.svg"
   >
     <template slot="action">
-      <div class="earn-mdt-reward-item-action">
-        <div class="earn-mdt-reward-item-action__amount-placholder">
-          <transition  
-            name="earn-mdt-reward-item-action__claim-button"    
-            @after-leave="afterLeave" 
-          >
-            <MDTSecondaryButton
-              v-show="!claimed"
-              ref="claimButton"
-              :style="claimButtonStyle"
-              class="earn-mdt-reward-item-action__claim-button"
-              color="secondary"
-              @click.stop="handleClickClaimButton"
-            >
-              {{ buttonText + " " + amountText }}
-            </MDTSecondaryButton>
-          </transition>
-          <div class="earn-mdt-reward-item-action__amount-wrapper">
-            <transition name="earn-mdt-reward-item-action__amount">
-              <MDTSecondaryButton 
-                v-if="showAmount" 
-                class="earn-mdt-reward-item-action__amount" 
-                disabled
-              >{{ `+${amountText}` }}</MDTSecondaryButton>
-            </transition>
-            <transition 
-              v-for="n in 8" 
-              :key="n" 
-              name="deco"
-            >    
-              <span  
-                v-if="showAmount" 
-                :class="['deco', 'deco-' + n]"
-              />
-            </transition> 
-          </div>
-        </div>
+      <div class="claim-button-wrapper">
+        <ClaimButton 
+          :reward-status="reward.status"
+          :amount="reward.value"
+          :currency="reward.currency"
+          :expiry-in-days="expiryInDays"
+          :expiry-time="reward.expiry_time"
+          @click.stop="handleClickClaimButton"
+        />
       </div>
     </template>
   </BaseEarnMDTItem>
@@ -56,13 +28,14 @@ import { trackEvent } from '@/utils';
 
 import BaseEarnMDTItem from '@/components/task/BaseEarnMDTItem';
 import MDTSecondaryButton from '@/components/button/MDTSecondaryButton';
+import ClaimButton from '@/components/button/ClaimButton';
 import { CLAIM_REWARD } from '@/store/modules/entities/rewards';
-import { formatAmount } from '@/utils';
 
 export default {
   components: {
     BaseEarnMDTItem,
     MDTSecondaryButton,
+    ClaimButton,
   },
   props: {
     reward: {
@@ -76,46 +49,33 @@ export default {
   },
   data() {
     return {
-      claimButtonStyle: null,
-      // animationPlaceholderStyle: null,
-      showAmount: false,
+      claimed: false,
     };
   },
   computed: {
-    claimed() {
-      return this.reward.status === 4;
-    },
-    buttonText() {
-      return this.claimed ? '' : this.$t('message.earnMDT.claim');
-    },
-    amountText() {
-      return `${formatAmount(this.reward.value, { type: 'short' })} ${
-        this.reward.currency
-      }`;
-    },
-  },
-  created() {
-    if (this.reward.status === 4) {
-      this.showAmount = true;
-    }
-  },
-  mounted() {
-    this.$nextTick(() => {
-      this.claimButtonStyle = {
-        height: `${this.$refs.claimButton.$el.clientHeight}px`,
-        width: `${this.$refs.claimButton.$el.clientWidth}px`,
-      };
-    });
+    expiryInDays() {
+      if (!this.reward.expiry_time) {
+        return '';
+      }
 
-    // TODO: is more fine-tuned needed?
-    // const style = this.$refs.claimButton.$el.currentStyle || window.getComputedStyle(this.$refs.claimButton.$el);
-    // const marginVertical = parseInt(style.marginTop, 10) + parseInt(style.marginRight, 10);
-    // const marginHorizontal = parseInt(style.marginLeft, 10) + parseInt(style.marginRight, 10);
+      const expiryDate = new Date(this.reward.expiry_time);
+      const now = new Date();
+      const utc1 = Date.UTC(
+        expiryDate.getFullYear(),
+        expiryDate.getMonth(),
+        expiryDate.getDate(),
+      );
+      const utc2 = Date.UTC(now.getFullYear(), now.getMonth(), now.getDate());
 
-    // this.animationPlaceholderStyle = {
-    //   height: `${this.$refs.claimButton.$el.clientHeight + marginVertical}px`,
-    //   width: `${this.$refs.claimButton.$el.clientWidth + marginHorizontal}px`,
-    // };
+      // https://stackoverflow.com/a/15289883
+      const dateDiffInDays = Math.floor((utc1 - utc2) / (1000 * 60 * 60 * 24));
+
+      if (dateDiffInDays >= 0) {
+        return dateDiffInDays;
+      }
+
+      return 0;
+    },
   },
   methods: {
     ...mapActions({
@@ -129,9 +89,6 @@ export default {
         rewardId: this.reward.id,
         userId: this.userId,
       });
-    },
-    afterLeave() {
-      this.showAmount = true;
     },
   },
 };
@@ -148,208 +105,9 @@ export default {
       fill: $theme-secondary-color;
     }
   }
-
-  .earn-mdt-reward-item-action {
-    justify-content: flex-end;
-    display: flex;
-
-    .earn-mdt-reward-item-action__claim-button {
-      margin-right: 0px;
-      margin-left: 0px;
-    }
-
-    .earn-mdt-reward-item-action__claim-button-leave-active {
-      min-width: 0;
-      animation-name: shrink;
-      animation-duration: 0.4s;
-      animation-timing-function: linear;
-      animation-fill-mode: forwards;
-    }
-
-    .earn-mdt-reward-item-action__amount-to-earn {
-      font-size: 0.875rem;
-      font-weight: bold;
-      color: $theme-secondary-color;
-      margin-right: 0.625rem;
-    }
-
-    .earn-mdt-reward-item-action__amount-to-earn-leave-active {
-      animation-name: earn-mdt-reward-item-action__amount-to-earn-animation;
-      animation-duration: 0.4s;
-      animation-timing-function: linear;
-      animation-fill-mode: forwards;
-    }
-
-    .earn-mdt-reward-item-action__amount-placholder {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      margin-left: auto;
-      line-height: 1.25rem;
-
-      .earn-mdt-reward-item-action__amount-wrapper {
-        position: relative;
-
-        .earn-mdt-reward-item-action__amount {
-          background-color: transparent;
-          color: $theme-secondary-color;
-          min-width: 0;
-          font-weight: bold;
-          box-shadow: none;
-
-          /deep/ .md-ripple {
-            padding: 0;
-          }
-        }
-
-        .earn-mdt-reward-item-action__amount-enter-active {
-          animation-name: zoomIn;
-          animation-duration: 0.5s;
-          animation-fill-mode: forwards;
-        }
-
-        .deco-enter-active {
-          animation-duration: 650ms;
-          animation-delay: 150ms;
-          animation-fill-mode: forwards;
-
-          $positionLeftOfCenterPoint: 75%;
-          $positionTopOfCenterPoint: 50%;
-          $horizontalOffset: 8%;
-          &.deco-1 {
-            animation-name: vanish, moveToTop;
-            top: ($positionTopOfCenterPoint - 30%);
-            left: $positionLeftOfCenterPoint;
-            transform: rotate(90deg);
-            transform-origin: left;
-          }
-
-          &.deco-2 {
-            top: ($positionTopOfCenterPoint - 20%);
-            right: (100% - $positionLeftOfCenterPoint) - ($horizontalOffset/2);
-            animation-name: vanish, moveToTop, moveToRight;
-            transform: rotate(-45deg);
-            transform-origin: right top;
-          }
-
-          &.deco-3 {
-            top: $positionTopOfCenterPoint;
-            right: (100% - $positionLeftOfCenterPoint) - $horizontalOffset;
-            animation-name: vanish, moveToRight;
-            // transform: translateY(-50%);
-          }
-
-          &.deco-4 {
-            top: ($positionTopOfCenterPoint + 20%);
-            right: (100% - $positionLeftOfCenterPoint) - ($horizontalOffset/2);
-            transform: rotate(45deg);
-            animation-name: vanish, moveToBottom, moveToRight;
-            transform-origin: right bottom;
-          }
-
-          &.deco-5 {
-            top: ($positionTopOfCenterPoint + 30%);
-            left: $positionLeftOfCenterPoint;
-            transform: rotate(-90deg);
-            animation-name: vanish, moveToBottom;
-            transform-origin: left;
-          }
-
-          &.deco-6 {
-            top: ($positionTopOfCenterPoint + 20%);
-            left: ($positionLeftOfCenterPoint - $horizontalOffset/2);
-            transform: rotate(-45deg);
-            transform-origin: left bottom;
-            animation-name: vanish, moveToBottom, moveToLeft;
-          }
-
-          &.deco-7 {
-            top: $positionTopOfCenterPoint;
-            left: $positionLeftOfCenterPoint - $horizontalOffset;
-            animation-name: vanish, moveToLeft;
-          }
-
-          &.deco-8 {
-            top: ($positionTopOfCenterPoint - 20%);
-            left: ($positionLeftOfCenterPoint - $horizontalOffset/2);
-            transform: rotate(45deg);
-            transform-origin: left top;
-            animation-name: vanish, moveToTop, moveToLeft;
-          }
-        }
-
-        .deco {
-          height: 1px;
-          width: 0px;
-          background-color: $theme-secondary-color;
-          position: absolute;
-        }
-      }
-    }
-  }
 }
 
-@keyframes shrink {
-  65% {
-    width: 24px;
-    transform: scale3d(1, 1, 1);
-  }
-  to {
-    width: 24px;
-    transform: scale3d(0, 0, 0);
-  }
-}
-
-@keyframes earn-mdt-reward-item-action__amount-to-earn-animation {
-  from {
-    opacity: 1;
-  }
-  to {
-    opacity: 0;
-  }
-}
-
-@keyframes vanish {
-  from {
-    width: 8px;
-  }
-  to {
-    width: 0px;
-  }
-}
-@keyframes moveToTop {
-  to {
-    margin-top: -10px;
-  }
-}
-@keyframes moveToRight {
-  to {
-    margin-right: -10px;
-  }
-}
-@keyframes moveToBottom {
-  to {
-    margin-top: 10px;
-  }
-}
-@keyframes moveToLeft {
-  to {
-    margin-left: -10px;
-  }
-}
-@keyframes zoomIn {
-  from {
-    opacity: 0;
-    transform: scale3d(0.3, 0.3, 0.3);
-  }
-  50% {
-    opacity: 1;
-  }
-  75% {
-    transform: scale3d(1.1, 1.1, 1.1);
-  }
-  100% {
-    transform: scale3d(1, 1, 1);
-  }
+.claim-button-wrapper {
+  text-align: right;
 }
 </style>
