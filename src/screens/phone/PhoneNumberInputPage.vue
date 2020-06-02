ad1e7af75cd29ef4532fba452396c293c2631ba8
Cho Chi Him <chochihim@gmail.com>
2020-06-02 12:22:11 +0800

    Ensure pincode is set in phone number setup process

--- src/screens/phone/PhoneNumberInputPage.vue
+++ src/screens/phone/PhoneNumberInputPage.vue
@@ -1,22 +1,31 @@
 <template>
-  <BasePhoneNumberInputPage 
-    v-bind="$attrs"
-    :title="$t('message.phone.add_phone_title')"
-    :content="$t('message.phone.add_phone_content')"
-    @nextClick="onNextClicked"
-  />
+  <div>
+    <BasePhoneNumberInputPage 
+      v-bind="$attrs"
+      :title="$t('message.phone.add_phone_title')"
+      :content="$t('message.phone.add_phone_content')"
+      @nextClick="onNextClicked"
+    />
+    <PinCodePopup
+      :md-active.sync="showPinCode"
+      :pin-setup-content="$t('message.ethBinding.pinSetupPopupDescription')"
+      @codefilled="onPinCodeFilled"
+    />
+  </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions, mapState } from 'vuex';
 import { RouteDef } from '@/constants';
 import { REQUEST_VERIFICATION_CODE } from '@/store/modules/security';
+import PinCodePopup from '@/components/popup/PinCodePopup';
 import BasePhoneNumberInputPage from '@/screens/phone/BasePhoneNumberInputPage';
 import BasePage from '@/screens/BasePage';
 
 export default {
   components: {
     BasePhoneNumberInputPage,
+    PinCodePopup,
   },
   extends: BasePage,
   metaInfo() {
@@ -24,11 +33,29 @@ export default {
       title: this.$t('message.settings.phoneNumber'),
     };
   },
+  data() {
+    return {
+      showPinCode: false,
+    };
+  },
+  computed: {
+    ...mapState({
+      pin: state => state.security.pin,
+    }),
+  },
   methods: {
     ...mapActions({
       requestVerificationCode: REQUEST_VERIFICATION_CODE,
     }),
     async onNextClicked() {
+      if (!this.pin) {
+        this.showPinCode = true;
+        return;
+      }
+
+      this.onPinCodeFilled();
+    },
+    async onPinCodeFilled() {
       const action = this.$route.params.action;
       await this.requestVerificationCode({
         action,
